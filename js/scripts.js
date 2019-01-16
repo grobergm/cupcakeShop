c4b18f0070ad9a37bc971eb7d496d2226c37101a
Ian Cook Westgate and Matt Groberg <matt@>
2019-01-16 15:35:44 -0800

    add more jQuery ux design

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,13 @@
 $(document).ready(function(){
-  $(".review").slideDown(4000);
+  $(".faaast").slideDown(2000);
+  $(".slooow").delay(2000).slideDown(8000);
   $(".review").click(function(){
     $(this).fadeToggle("slow");
   });
   $("#listen").click(function(){
     $(".review").fadeIn("slow");
-  })
+  });
+  $("#backgroundChanger").click(function(){
+    $("body").css("background-image","url('Images/cookieBackground.jpeg')")
+  });
 })
